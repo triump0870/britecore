c12aa34cf7def027b3075a481f48a13d91ada678
triump0870 <b4you0870@gmail.com>
2018-02-10 18:23:34 +0530

    Add the url configuration

--- apis/urls.py
+++ apis/urls.py
@@ -1,4 +1,15 @@
 from django.conf.urls import url
+from apis import views
 
 urlpatterns = [
+    url(r'^$', views.api_root, name='api-root'),
+    url(r'^risks/$',
+        views.RiskListCreateAPI.as_view(),
+        name='risks'),
+    url(r'^risktypes/$',
+        views.RiskTypeListCreateAPI.as_view(),
+        name='risk_types'),
+    url(r'^riskfields/$',
+        views.RiskFieldListCreateAPI.as_view(),
+        name='risk_fields'),
 ]
