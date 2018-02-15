f6c4e077cde73d63d7e32a81485d23589d5aa359
triump0870 <b4you0870@gmail.com>
2018-02-16 03:33:15 +0530

    Update RiskTypeSerializer with create and update method

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,12 +1,16 @@
-from rest_framework.serializers import ModelSerializer, RelatedField
-from app.models import RiskType, RiskField, Risk
 import json
+
+from rest_framework.serializers import ModelSerializer
+from django.utils.text import slugify
+from django.db.models import Q
+
+from app.models import RiskType, RiskField, Risk
 
 
 class RiskFieldSerializer(ModelSerializer):
     class Meta:
         model = RiskField
-        fields = ('name', 'type')
+        fields = ('name', 'slug', 'type')
 
 
 class RiskTypeSerializer(ModelSerializer):
@@ -15,6 +19,31 @@ class RiskTypeSerializer(ModelSerializer):
     class Meta:
         model = RiskType
         fields = ('id', 'description', 'name', 'risk_field_list')
+
+    def create(self, validated_data):
+        risk_field_list = validated_data.pop('risk_fields')
+        instance = RiskType.objects.create(**validated_data)
+        data = []
+        for item in risk_field_list:
+            name = item.pop('name')
+            obj = RiskField.objects.filter(Q(slug=slugify(name)) | Q(name=name)).first()
+            if obj is not None:
+                data.append(obj.id)
+        instance.risk_fields = data
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        risk_field_list = validated_data.pop('risk_fields')
+        data = []
+        for item in risk_field_list:
+            name = item.pop('name')
+            obj = RiskField.objects.filter(Q(slug=slugify(name)) | Q(name=name)).first()
+            if obj is not None:
+                data.append(obj.id)
+        instance.risk_fields = data
+        instance.save()
+        return instance
 
 
 class RiskSerializer(ModelSerializer):
